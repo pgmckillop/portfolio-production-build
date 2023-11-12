937cbe98e0dd82c90dbb3b0ec5c725eeaf1aa641
Paul McKillop <pgmckillop@gmail.com>
2023-11-12 18:11:48 +0000

    Phase 1 complete

--- site_admin/projects/views.py
+++ site_admin/projects/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'projects/index.html', {})
+
+
+# about
+def about(request):
+    return render(request, "projects/about.html", {})
